96766edcc25f1ac55ee6109ae35477d120b5a1bf
Heidi <hengstrom25@gmail.com>
2018-09-11 23:12:40 -0400

    finish UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
 
-	#gets '/users/:id' do
-		#if !logged_in?
-			#redirect '/'
-		#end
-	#end
-		
-	#@user = User.find(params[:id])
-	#if !@user.nil? && @user == current_user
-		#erb :'users/show'
-	#else
-		#redirect '/'
-	#end
+	gets '/users/:id' do
+		if !logged_in?
+			redirect '/'
+		else 
+			@user = User.find(params[:id])
+			if !@user.nil? && @user == current_user
+				erb :'users/show'
+			else
+				redirect '/'
+			end	
+		end
+	end
 	
 	get '/signup' do
 		if !session[:user_id]
@@ -31,15 +31,32 @@ class UsersController < ApplicationController
 		end
 	end
 	
-	#get '/login' do
-		#user = User.find_by(:username => params[:username])
-			#if user && user.authenticate(params[:password])
-				#session[:user_id] = user.id
-				#redirect '/beers'
-			#else
-				#redirect '/signup'
-			#end
-	#end
-				
+	get '/login' do
+		if !session[:user_id]
+			erb :'users/login'
+		else	
+			redirect '/beers'
+		end
+	end
+			
+	
+	post '/login' do
+		user = User.find_by(:username => params[:username])
+			if user && user.authenticate(params[:password])
+				session[:user_id] = user.id
+				redirect '/beers'
+			else
+				redirect '/signup'
+			end
+	end
+	
+	get '/logout' do
+		if session[:user_id] !=nil
+			session.destroy
+			redirect '/login'
+		else
+			redirect '/'
+		end
+	end			
 	
 end
